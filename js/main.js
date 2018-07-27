24b7d5ad0b3367d00b80af8996337739d4a0c47d
Alex Yeahnotov <yeahhh.not@gmail.com>
2018-07-27 18:23:35 +0300

    Страница Media - Video - стилизация, fix разметки, fix Page title. Fix остающийся focus на кнопках после клика, удалил focus=hover стилизацию.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,24 @@ var mainNavToggle = document.querySelector('.main-nav__toggle');
 
 page.classList.remove('no-js');
 
+// document.addEventListener('click', function(e) {
+//   if(document.activeElement.toString() == '[object HTMLButtonElement]'){
+//     document.activeElement.blur();
+//   }
+// });
+
+page.addEventListener('mousedown', function(){
+  if (!page.classList.contains('no-focus')) {
+    page.classList.add('no-focus');
+  }
+});
+
+page.addEventListener('keydown', function(){
+  if (page.classList.contains('no-focus')) {
+    page.classList.remove('no-focus');
+  }
+});
+
 mainNavToggle.addEventListener('click', function(){
   if (mainNav.classList.contains('main-nav--closed')) {
     mainNav.classList.remove('main-nav--closed');
